520746fe04c2fbc0dddf0cc84031e9aade196f09
Krish Bhanushali <krishbhanushali@Krishs-MacBook-Pro.local>
2021-03-26 00:13:46 -0700

    added First App

---
+++ I am rich afaf/I am rich afaf/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am rich afaf
+//
+//  Created by Krish Bhanushali on 25/03/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
